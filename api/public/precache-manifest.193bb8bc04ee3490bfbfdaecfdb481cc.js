eb5d23f59e212f2895d7bf426c6242d0c0891164
Alex Shi <alexshi@huntsman-ve701-0304.eduroam.wlan.private.upenn.edu>
2019-09-07 10:13:29 -0400

    updated package.json

---
+++ api/public/precache-manifest.193bb8bc04ee3490bfbfdaecfdb481cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23baf4a0c851f3e264310aa7d974d2b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a692d85fd16841966ca1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4ec87e73d32f96931e30",
+    "url": "/static/js/2.11de0b1d.chunk.js"
+  },
+  {
+    "revision": "a692d85fd16841966ca1",
+    "url": "/static/js/main.83c961cd.chunk.js"
+  },
+  {
+    "revision": "679727bf39c2da063136",
+    "url": "/static/js/runtime~main.2ed0fb32.js"
+  }
+]);
