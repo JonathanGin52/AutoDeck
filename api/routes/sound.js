c854b20c78808886e19636cd72e1ecfba9dfdad2
Rui Li <yuanruili2000@gmail.com>
2019-09-07 20:39:21 -0400

    api functions

--- api/routes/sound.js
+++ api/routes/sound.js
@@ -1,49 +1,69 @@
 const language = require('@google-cloud/language');
-const nlp_client = new language.LanguageServiceClient();
+const NLPClient = new language.LanguageServiceClient();
 const fetch = require('node-fetch');
+const SlideFunctions = require('../SlideFunctions');
 var express = require('express');
 var router = express.Router();
 
 router.post('/api/record', (req, res, next) => {
   const transcript = req.body.transcript;
   fetch('http://localhost:8080/nlp/entity_sentiments', {
-        method: 'POST',
+    method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ text: transcript }),
   }).then(r => {
-    return r.json();
-  }).then(r => {
+    r.json().then(resp => {
     // Todo logic for updating the slides
-    console.log(r);
-  });
-  res.status(200);
+    console.log(resp);
+    if (transcript.toLowerCase() == 'next slide') {
+      SlideFunctions.createSlide({});
+    }
+  })});
 });
+
+function authors() {}
+
+function two() {}
+
+function three() {}
+
+function four() {}
+
+function five() {]
+
+function six() {}
+
+function image() {}
+
+// ========================================
+// NLP stuff
 
 router.post('/nlp/entity_sentiments', function(req, res, next) {
-  nlp_request(req, res);
+  nlpRequest(req, res);
 });
 
-function return_response(res, data) {
+function returnResponse(res, data) {
   res.json(JSON.stringify(data));
 }
 
-function nlp_request(req, res) {
+function nlpRequest(req, res) {
   const text = req.body.text;
 
   const document = {
     content: text,
     type: 'PLAIN_TEXT',
   };
-  nlp_client
+
+  NLPClient
     .analyzeEntitySentiment({document})
     .then(results => {
-      return_response(res, results[0]);
+      returnResponse(res, results[0]);
     })
     .catch(err => {
-      return_response(res, { error: err });
+      returnResponse(res, { error: err.message });
     });
 }
 
